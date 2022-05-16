c99bd782984bb3968b7300cc6aaef37487854969
Jesse Kercheval <jesse.kercheval@gmail.com>
2022-05-16 17:48:36 +0000

    update for work on 5.16

---
+++ ruby_basics/rubybasics_exercise1.rb
@@ -0,0 +1,14 @@
+# Jesse Kercheval
+# May 16, 2022
+# RB100
+
+n = 1
+
+while n <= 10
+  puts n
+  n += 1
+  break if n == 5
+end
+
+puts ""
+p n
---
+++ ruby_basics/rubybasics_exercise6.rb
@@ -0,0 +1,10 @@
+# Jesse Kercheval
+# May 16, 2022
+# RB100
+
+puts "Hi, what's your name? I'll output your symbol: "
+name = gets.chomp.to_sym
+p name
+
+#submitted my :"Jesse Kercheval" result to LS because it was different from the :Jesse they recommended
+# no biggie
